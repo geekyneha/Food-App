90045602d66321bf4fbd33ec22ce74f86989034b
Neha <nehatualea@gmail.com>
2025-06-17 22:04:10 +0530

    EP 04:  Header, Body, Res Card

--- App.js
+++ App.js
@@ -1,16 +1,671 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="logo">
+        <img
+          src="https://img.freepik.com/premium-vector/logo-food-company-that-says-sun-sun-sunflower_917213-253424.jpg?semt=ais_hybrid&w=740"
+          width={60}
+          alt="logo"
+        />
+      </div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+const ResCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, costForTwo, avgRating, cuisines, sla } =
+    resData?.info;
+  return (
+    <div className="res-card">
+      <div className="res-logo">
+        <img
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+            cloudinaryImageId
+          }
+          alt="logo"
+        />
+      </div>
+      <h2>{name}</h2>
+      <h2>{cuisines.join(", ")}</h2>
+      <h3>{costForTwo}</h3>
+      <h3>{avgRating}</h3>
+      <h3>{sla.deliveryTime} minutes</h3>
+    </div>
+  );
+};
+
+//   {
+//     info: {
+//       id: "151656",
+//       name: "Dev International",
+//       cloudinaryImageId: "enj3srsnhbltbom2ovvh",
+//       locality: "khajri road",
+//       areaName: "Mohan Nagar",
+//       costForTwo: "₹100 for two",
+//       cuisines: ["North Indian", "Chinese", "Fast Food", "Beverages"],
+//       avgRating: 4.3,
+//       parentId: "71556",
+//       avgRatingString: "4.3",
+//       totalRatingsString: "520",
+//       sla: {
+//         deliveryTime: 54,
+//         lastMileTravel: 13.6,
+//         serviceability: "SERVICEABLE",
+//         slaString: "50-55 mins",
+//         lastMileTravelString: "13.6 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-17 22:30:00",
+//         opened: true,
+//       },
+//       badges: {},
+//       isOpen: true,
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {},
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       aggregatedDiscountInfoV3: {
+//         header: "₹100 OFF",
+//         subHeader: "ABOVE ₹299",
+//         discountTag: "FLAT DEAL",
+//       },
+//       orderabilityCommunication: {
+//         title: {},
+//         subTitle: {},
+//         message: {},
+//         customIcon: {},
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           mediaType: "ADS_MEDIA_ENUM_IMAGE",
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//     analytics: {
+//       context: "seo-data-4f7de361-97ba-453b-bd33-9331bb0e45d1",
+//     },
+//     cta: {
+//       link: "https://www.swiggy.com/city/chhindwara/dev-international-khajri-road-mohan-nagar-rest151656",
+//       text: "RESTAURANT_MENU",
+//       type: "WEBLINK",
+//     },
+//     widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+//   },
+//   {
+//     info: {
+//       id: "658210",
+//       name: "The Fusion Lounge",
+//       cloudinaryImageId: "fa4944f0cfdcbca2bec1f3ab8e3db3f7",
+//       locality: "Triloki nagar",
+//       areaName: "Railway Station",
+//       costForTwo: "₹300 for two",
+//       cuisines: [
+//         "South Indian",
+//         "Chinese",
+//         "Beverages",
+//         "Fast Food",
+//         "Desserts",
+//       ],
+//       avgRating: 4.2,
+//       parentId: "395453",
+//       avgRatingString: "4.2",
+//       totalRatingsString: "350",
+//       sla: {
+//         deliveryTime: 52,
+//         lastMileTravel: 10.4,
+//         serviceability: "SERVICEABLE",
+//         slaString: "50-55 mins",
+//         lastMileTravelString: "10.4 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-17 23:00:00",
+//         opened: true,
+//       },
+//       badges: {},
+//       isOpen: true,
+//       aggregatedDiscountInfoV2: {},
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {},
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//     analytics: {
+//       context: "seo-data-4f7de361-97ba-453b-bd33-9331bb0e45d1",
+//     },
+//     cta: {
+//       link: "https://www.swiggy.com/city/chhindwara/the-fusion-lounge-triloki-nagar-railway-station-rest658210",
+//       type: "WEBLINK",
+//     },
+//   },
+// ];
+
+const resList = [
+  {
+    info: {
+      id: "151656",
+      name: "Dev International",
+      cloudinaryImageId: "enj3srsnhbltbom2ovvh",
+      locality: "khajri road",
+      areaName: "Mohan Nagar",
+      costForTwo: "₹100 for two",
+      cuisines: ["North Indian", "Chinese", "Fast Food", "Beverages"],
+      avgRating: 4.3,
+      parentId: "71556",
+      avgRatingString: "4.3",
+      totalRatingsString: "520",
+      sla: {
+        deliveryTime: 54,
+        lastMileTravel: 13.6,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "13.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-17 22:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹299",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "658210",
+      name: "The Fusion Lounge",
+      cloudinaryImageId: "fa4944f0cfdcbca2bec1f3ab8e3db3f7",
+      locality: "Triloki nagar",
+      areaName: "Railway Station",
+      costForTwo: "₹300 for two",
+      cuisines: ["Chinese", "Beverages", "Desserts"],
+      avgRating: 4.2,
+      parentId: "395453",
+      avgRatingString: "4.2",
+      totalRatingsString: "350",
+      sla: {
+        deliveryTime: 52,
+        lastMileTravel: 10.4,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "10.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-17 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "184424",
+      name: "Sab Ghar Tak Foods",
+      cloudinaryImageId: "ryzswg44bt7thqzkuaj4",
+      locality: "Sanchar Colony",
+      areaName: "Parsia Road",
+      costForTwo: "₹200 for two",
+      cuisines: ["Chinese", "Beverages", "Fast Food"],
+      avgRating: 4.1,
+      parentId: "173932",
+      avgRatingString: "4.1",
+      totalRatingsString: "617",
+      sla: {
+        deliveryTime: 59,
+        lastMileTravel: 14.1,
+        serviceability: "SERVICEABLE",
+        slaString: "55-60 mins",
+        lastMileTravelString: "14.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-17 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹50 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "385824",
+      name: "The Belgian Waffle Co.",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/17/a38d20d7-bbb7-4b67-8bdd-7740e85cd4af_385824.JPG",
+      locality: "Sinchai Colony",
+      areaName: "Mohan Nagar",
+      costForTwo: "₹200 for two",
+      cuisines: ["Waffle", "Desserts", "Ice Cream", "Beverages"],
+      avgRating: 4.7,
+      veg: true,
+      parentId: "2233",
+      avgRatingString: "4.7",
+      totalRatingsString: "312",
+      sla: {
+        deliveryTime: 52,
+        lastMileTravel: 13.4,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "13.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-17 22:45:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "1003414",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/a1e023e4-bdf3-4bcb-b4d9-2498ba52528e_1003414.JPG",
+      locality: "Chhindwara",
+      areaName: "Parasia Road",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "124",
+      sla: {
+        deliveryTime: 53,
+        lastMileTravel: 14.3,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "14.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-18 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "234875",
+      name: "Adil Hotel",
+      cloudinaryImageId: "gp1ityra6utvzqn6ghnv",
+      locality: "Rautha Wada",
+      areaName: "Chhindwara Locality",
+      costForTwo: "₹150 for two",
+      cuisines: ["North Indian", "Biryani", "Tandoor"],
+      avgRating: 4.3,
+      parentId: "27123",
+      avgRatingString: "4.3",
+      totalRatingsString: "1.3K+",
+      sla: {
+        deliveryTime: 40,
+        lastMileTravel: 11.5,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "11.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-18 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹129",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "151518",
+      name: "Bakery World",
+      cloudinaryImageId: "mt2aggiscfl3yviatwng",
+      locality: "Parasia Road",
+      areaName: "Parasia Road",
+      costForTwo: "₹250 for two",
+      cuisines: ["Bakery", "Ice Cream", "Snacks", "Beverages"],
+      avgRating: 4.4,
+      veg: true,
+      parentId: "40363",
+      avgRatingString: "4.4",
+      totalRatingsString: "270",
+      sla: {
+        deliveryTime: 40,
+        lastMileTravel: 12.4,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "12.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-17 22:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <>
+      <div className="body-container">
+        {resList.map((restaurant) => {
+          return <ResCard key={restaurant.info.id} resData={restaurant} />;
+        })}
+      </div>
+    </>
+  );
+};
 const AppLayout = () => {
   return (
-    <div>
-      <h1>Food App</h1>
-      <p>Welcome to the Food App!</p>
-      <p>This is a simple React application.</p>
-      <p>Enjoy your meal!</p>
+    <div className="app-layout">
+      <Header />
+      <Body />
     </div>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
-export default AppLayout;
