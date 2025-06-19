81044b991f33d6e6e2c9548c0a21ed3d4f70af87
Neha <nehatualea@gmail.com>
2025-06-19 22:40:14 +0530

    Restaurant Cards, API data fetch,

---
+++ src/components/ResCard.js
@@ -0,0 +1,46 @@
+import { CDN_URL } from "../utils/constant.js";
+const ResCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, costForTwo, avgRating, cuisines, sla } =
+    resData?.info;
+
+  // export default ResCard;
+
+  return (
+    <div className="card">
+      <div className="image-container">
+        <img
+          src={CDN_URL + cloudinaryImageId}
+          alt="food"
+          className="food"
+          width={295}
+        />
+        <p className="promoted">Promoted</p>
+
+        <p className="delivery-time">{sla.deliveryTime} min</p>
+      </div>
+      <div className="content">
+        <div className="name-rating">
+          <h3 className="restaurant">{name}</h3>
+          <div className="rating">{avgRating}</div>
+        </div>
+        <p className="cuisines">{cuisines.join(", ")}</p>
+        <div>{costForTwo}</div>
+
+        <hr />
+        <div className="max-safety">
+          <div className="container">
+            <div className="safety">max safety</div>
+            <div className="delivery">delivery</div>
+          </div>
+
+          <div className="about-safety">
+            Follows All Max Safety Measures to ensure your food is safe.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResCard;
---
+++ src/utils/mockData.js
@@ -0,0 +1,458 @@
+// let resList = [
+//   {
+//     info: {
+//       id: "151656",
+//       name: "Dev International",
+//       cloudinaryImageId: "enj3srsnhbltbom2ovvh",
+//       locality: "khajri road",
+//       areaName: "Mohan Nagar",
+//       costForTwo: "₹100 for two",
+//       cuisines: ["North Indian", "Chinese", "Fast Food", "Beverages"],
+//       avgRating: 3.3,
+//       parentId: "71556",
+//       avgRatingString: "4.3",
+//       totalRatingsString: "520",
+//       sla: {
+//         deliveryTime: 54,
+//         lastMileTravel: 13.6,
+//         serviceability: "SERVICEABLE",
+//         slaString: "50-55 mins",
+//         lastMileTravelString: "13.6 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-17 22:30:00",
+//         opened: true,
+//       },
+//       badges: {},
+//       isOpen: true,
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {},
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       aggregatedDiscountInfoV3: {
+//         header: "₹100 OFF",
+//         subHeader: "ABOVE ₹299",
+//         discountTag: "FLAT DEAL",
+//       },
+//       orderabilityCommunication: {
+//         title: {},
+//         subTitle: {},
+//         message: {},
+//         customIcon: {},
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           mediaType: "ADS_MEDIA_ENUM_IMAGE",
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//   },
+//   {
+//     info: {
+//       id: "658210",
+//       name: "The Fusion Lounge",
+//       cloudinaryImageId: "fa4944f0cfdcbca2bec1f3ab8e3db3f7",
+//       locality: "Triloki nagar",
+//       areaName: "Railway Station",
+//       costForTwo: "₹300 for two",
+//       cuisines: ["Chinese", "Beverages", "Desserts"],
+//       avgRating: 3.2,
+//       parentId: "395453",
+//       avgRatingString: "4.2",
+//       totalRatingsString: "350",
+//       sla: {
+//         deliveryTime: 52,
+//         lastMileTravel: 10.4,
+//         serviceability: "SERVICEABLE",
+//         slaString: "50-55 mins",
+//         lastMileTravelString: "10.4 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-17 23:00:00",
+//         opened: true,
+//       },
+//       badges: {},
+//       isOpen: true,
+//       aggregatedDiscountInfoV2: {},
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {},
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//   },
+//   {
+//     info: {
+//       id: "184424",
+//       name: "Sab Ghar Tak Foods",
+//       cloudinaryImageId: "ryzswg44bt7thqzkuaj4",
+//       locality: "Sanchar Colony",
+//       areaName: "Parsia Road",
+//       costForTwo: "₹200 for two",
+//       cuisines: ["Chinese", "Beverages", "Fast Food"],
+//       avgRating: 3.8,
+//       parentId: "173932",
+//       avgRatingString: "4.1",
+//       totalRatingsString: "617",
+//       sla: {
+//         deliveryTime: 59,
+//         lastMileTravel: 14.1,
+//         serviceability: "SERVICEABLE",
+//         slaString: "55-60 mins",
+//         lastMileTravelString: "14.1 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-17 23:00:00",
+//         opened: true,
+//       },
+//       badges: {},
+//       isOpen: true,
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {},
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       aggregatedDiscountInfoV3: {
+//         header: "₹50 OFF",
+//         subHeader: "ABOVE ₹199",
+//         discountTag: "FLAT DEAL",
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//   },
+//   {
+//     info: {
+//       id: "385824",
+//       name: "The Belgian Waffle Co.",
+//       cloudinaryImageId:
+//         "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/17/a38d20d7-bbb7-4b67-8bdd-7740e85cd4af_385824.JPG",
+//       locality: "Sinchai Colony",
+//       areaName: "Mohan Nagar",
+//       costForTwo: "₹200 for two",
+//       cuisines: ["Waffle", "Desserts", "Ice Cream", "Beverages"],
+//       avgRating: 3.8,
+//       veg: true,
+//       parentId: "2233",
+//       avgRatingString: "4.7",
+//       totalRatingsString: "312",
+//       sla: {
+//         deliveryTime: 52,
+//         lastMileTravel: 13.4,
+//         serviceability: "SERVICEABLE",
+//         slaString: "50-55 mins",
+//         lastMileTravelString: "13.4 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-17 22:45:00",
+//         opened: true,
+//       },
+//       badges: {
+//         imageBadges: [
+//           {
+//             imageId: "v1695133679/badges/Pure_Veg111.png",
+//             description: "pureveg",
+//           },
+//         ],
+//       },
+//       isOpen: true,
+//       aggregatedDiscountInfoV2: {},
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {
+//             badgeObject: [
+//               {
+//                 attributes: {
+//                   description: "pureveg",
+//                   imageId: "v1695133679/badges/Pure_Veg111.png",
+//                 },
+//               },
+//             ],
+//           },
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//   },
+//   {
+//     info: {
+//       id: "1003414",
+//       name: "Pizza Hut",
+//       cloudinaryImageId:
+//         "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/a1e023e4-bdf3-4bcb-b4d9-2498ba52528e_1003414.JPG",
+//       locality: "Chhindwara",
+//       areaName: "Parasia Road",
+//       costForTwo: "₹350 for two",
+//       cuisines: ["Pizzas"],
+//       avgRating: 4.2,
+//       parentId: "721",
+//       avgRatingString: "4.2",
+//       totalRatingsString: "124",
+//       sla: {
+//         deliveryTime: 53,
+//         lastMileTravel: 14.3,
+//         serviceability: "SERVICEABLE",
+//         slaString: "50-55 mins",
+//         lastMileTravelString: "14.3 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-18 00:00:00",
+//         opened: true,
+//       },
+//       badges: {},
+//       isOpen: true,
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {},
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       aggregatedDiscountInfoV3: {
+//         header: "ITEMS",
+//         subHeader: "AT ₹59",
+//       },
+//       orderabilityCommunication: {
+//         title: {},
+//         subTitle: {},
+//         message: {},
+//         customIcon: {},
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           mediaType: "ADS_MEDIA_ENUM_IMAGE",
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//   },
+//   {
+//     info: {
+//       id: "234875",
+//       name: "Adil Hotel",
+//       cloudinaryImageId: "gp1ityra6utvzqn6ghnv",
+//       locality: "Rautha Wada",
+//       areaName: "Chhindwara Locality",
+//       costForTwo: "₹150 for two",
+//       cuisines: ["North Indian", "Biryani", "Tandoor"],
+//       avgRating: 4.3,
+//       parentId: "27123",
+//       avgRatingString: "4.3",
+//       totalRatingsString: "1.3K+",
+//       sla: {
+//         deliveryTime: 40,
+//         lastMileTravel: 11.5,
+//         serviceability: "SERVICEABLE",
+//         slaString: "35-40 mins",
+//         lastMileTravelString: "11.5 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-18 00:00:00",
+//         opened: true,
+//       },
+//       badges: {},
+//       isOpen: true,
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {},
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       aggregatedDiscountInfoV3: {
+//         header: "ITEMS",
+//         subHeader: "AT ₹129",
+//       },
+//       orderabilityCommunication: {
+//         title: {},
+//         subTitle: {},
+//         message: {},
+//         customIcon: {},
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           mediaType: "ADS_MEDIA_ENUM_IMAGE",
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//   },
+//   {
+//     info: {
+//       id: "151518",
+//       name: "Bakery World",
+//       cloudinaryImageId: "mt2aggiscfl3yviatwng",
+//       locality: "Parasia Road",
+//       areaName: "Parasia Road",
+//       costForTwo: "₹250 for two",
+//       cuisines: ["Bakery", "Ice Cream", "Snacks", "Beverages"],
+//       avgRating: 4.4,
+//       veg: true,
+//       parentId: "40363",
+//       avgRatingString: "4.4",
+//       totalRatingsString: "270",
+//       sla: {
+//         deliveryTime: 40,
+//         lastMileTravel: 12.4,
+//         serviceability: "SERVICEABLE",
+//         slaString: "40-45 mins",
+//         lastMileTravelString: "12.4 km",
+//         iconType: "ICON_TYPE_EMPTY",
+//       },
+//       availability: {
+//         nextCloseTime: "2025-06-17 22:30:00",
+//         opened: true,
+//       },
+//       badges: {
+//         imageBadges: [
+//           {
+//             imageId: "v1695133679/badges/Pure_Veg111.png",
+//             description: "pureveg",
+//           },
+//         ],
+//       },
+//       isOpen: true,
+//       aggregatedDiscountInfoV2: {},
+//       type: "F",
+//       badgesV2: {
+//         entityBadges: {
+//           imageBased: {
+//             badgeObject: [
+//               {
+//                 attributes: {
+//                   description: "pureveg",
+//                   imageId: "v1695133679/badges/Pure_Veg111.png",
+//                 },
+//               },
+//             ],
+//           },
+//           textBased: {},
+//           textExtendedBadges: {},
+//         },
+//       },
+//       orderabilityCommunication: {
+//         title: {},
+//         subTitle: {},
+//         message: {},
+//         customIcon: {},
+//       },
+//       differentiatedUi: {
+//         displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//         differentiatedUiMediaDetails: {
+//           mediaType: "ADS_MEDIA_ENUM_IMAGE",
+//           lottie: {},
+//           video: {},
+//         },
+//       },
+//       reviewsSummary: {},
+//       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//       restaurantOfferPresentationInfo: {},
+//       externalRatings: {
+//         aggregatedRating: {
+//           rating: "--",
+//         },
+//       },
+//       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+//     },
+//   },
+// ];
